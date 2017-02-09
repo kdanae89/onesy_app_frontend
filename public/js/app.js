344a7a67f3bf71efd0549e7b44bfb364ccfa2f13
Kaylie Weable <kaylieweable@Kaylies-MacBook-Pro.local>
2017-02-09 09:56:57 -0800

    images are displaying time for drag and drop

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,35 @@
 var app = angular.module('ProjectOnesie', []);
 
+//main controller -------------------------->
 app.controller('mainController', ['$http', function($http) {
 
+  //my variables --------------------------->
   this.url = 'http://localhost:3000';
   this.user = {};
   this.onsey = {};
+  this.images = {};
+
   //Img info for appending white onesy on create
   var whiteOnesy = document.createElement('img');
   whiteOnesy.src = "http://www.clipartkid.com/images/363/baby-onesie-white-trans-free-images-at-clker-com-vector-clip-art-C1WPC8-clipart.png";
   whiteOnesy.setAttribute("height", "500");
   whiteOnesy.setAttribute("width", "500");
   whiteOnesy.setAttribute("id", "droppable");
+
   //Img info for appending pink onesy on create
   var pinkOnesy = document.createElement('img');
   pinkOnesy.src = "http://www.clipartkid.com/images/472/light-pink-bodysuit-short-sleeve-baby-n-toddler-8EkNsb-clipart.jpg";
   pinkOnesy.setAttribute("height", "500");
   pinkOnesy.setAttribute("height", "500");
   pinkOnesy.setAttribute("id", "droppable");
+
   //Img info for appending blue onesy
   var blueOnesy = document.createElement('img');
   blueOnesy.src = "https://www.towelsandhome.com/media/catalog/product/cache/1/image/040ec09b1e35df139433887a97daa66f/s/h/short_sleeve_creeper-baby_blue_1_1.jpg";
   blueOnesy.setAttribute("height", "500");
   blueOnesy.setAttribute("height", "500");
   blueOnesy.setAttribute("id", "droppable");
+
   //Img info for yellow onesy
   var yellowOnesy = document.createElement('img');
   yellowOnesy.src = "http://www.sawyoo.com/postpic/2015/04/blank-baby-onesie-template_476014.jpg";
@@ -31,7 +38,7 @@ app.controller('mainController', ['$http', function($http) {
   yellowOnesy.setAttribute("id", "droppable");
 
 
-
+//USER ROUTES -------------------------------->
 
   // Post request for login, adds token to local storage
   this.login = function(logUser) {
@@ -62,20 +69,6 @@ app.controller('mainController', ['$http', function($http) {
     }.bind(this));
   }
 
-  // // Get request here!!!!!! Gets current user
-  // this.showUser = function() {
-  //   $http({
-  //     method: 'GET',
-  //     url: this.url + '/users/' + this.user.id,
-  //     headers: {'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token'))}
-  //   }).then(function(response) {
-  //     console.log(response);
-  //     this.user = response.data.username;
-  //   }.bind(this));
-  // }
-
-
-
   // Put request to edit a user's info
   this.edit = function(editMe) {
     console.log("checking user in edit", this.user.id);
@@ -90,6 +83,8 @@ app.controller('mainController', ['$http', function($http) {
       document.getElementById("editUser").reset();
     }.bind(this));
   }
+
+//ONESEY ROUTES --------------------------------->
 
 // Create a new onesy for a user
   this.createOnesy = function(newOnesy) {
@@ -113,5 +108,17 @@ app.controller('mainController', ['$http', function($http) {
     }.bind(this));
   }
 
+//IMAGE ROUTES --------------------------------->
+//image index, show me the images!!!!
+  this.allImages = function() {
+    $http({
+      method: 'GET',
+      url: this.url + '/images'
+    }).then(function(response) {
+      this.images = response.data.images;
+      // console.log(this.images);
+    }.bind(this));
+  }
+  this.allImages();
 
 }]);
